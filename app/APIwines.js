a1bd23d5c5e6736b384e11eb1689ca2a11c08936
Emanuel Puscalau <emi.puscalau@gmail.com>
2016-05-18 21:25:56 +0200

    api get all and post new

--- app/APIwines.js
+++ app/APIwines.js
@@ -10,7 +10,6 @@ let collection = req => req.db.collection('wines');
 
 let respondError = (errors, res, next) => {
   console.log(errors);
-
   for (let key in errors) {
     if (errors.hasOwnProperty(key)) {
       errors[key] = errors[key][0];
@@ -76,15 +75,25 @@ let constraintsFilters = {
   },
 };
 
+let getNextWineId = db =>
+  db.collection('counters').findOneAndUpdate(
+    { _id: 'wines' }, // filter
+    { $inc: { seq: 1 } }, // update
+    { upsert: true, returnOriginal: false} // options
+  );
+
 APIwines.showAll = (req, res, next) => {
   let errors = validate(req.query, constraintsFilters);
   if (errors) {
     return respondError(errors, res, next);
   }
-  let filters = validate.cleanAttributes(req.query, constraintsInsert);
-  console.log(filters);
 
-  return collection(req).find().toArray()
+  let filters = validate.cleanAttributes(req.query, constraintsFilters);
+  if (filters.hasOwnProperty('year'))  {
+    filters.year = parseInt(filters.year);
+  }
+
+  return collection(req).find(filters, { _id: 0 }).toArray()
     .then(docs=>{
       res.json(docs);
     })
@@ -103,10 +112,14 @@ APIwines.insertOne = (req, res, next) => {
   }
 
   let newWine = validate.cleanAttributes(req.body, constraintsInsert);
-
-  return collection(req).insertOne(newWine)
+  return getNextWineId(req.db)
+    .then( r => {
+      newWine.id = r.value.seq;
+      return collection(req).insertOne(newWine);
+    })
     .then(r=>{
       assert.equal(1, r.insertedCount);
+      delete r.ops[0]._id;
       res.json(r.ops[0]);
     })
     .catch(err => {
@@ -117,8 +130,8 @@ APIwines.insertOne = (req, res, next) => {
     });
 };
 
-// APIwines.updateOne(req, res, next) {};
-// APIwines.showOne(req, res, next) {};
-// APIwines.deleteOne(req, res, next) {};
+// APIwines.updateOne = (req, res, next) => {};
+// APIwines.showOne(req, res, next) => {};
+// APIwines.deleteOne(req, res, next) => {};
 
 module.exports = APIwines;
