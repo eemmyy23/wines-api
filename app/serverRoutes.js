aaafa3de759aac4112cb55ea5578793049911b18
Emanuel Puscalau <emi.puscalau@gmail.com>
2016-05-21 00:02:25 +0200

    code cleanup

--- app/serverRoutes.js
+++ app/serverRoutes.js
@@ -1,18 +1,10 @@
 let restify = require('restify');
-// let restifyValidator = require('restify-validator');
-
 let APIwines = require('./APIwines');
 
 module.exports = (server) => {
   server.use(restify.queryParser());
   server.pre(restify.pre.sanitizePath());
   server.use(restify.bodyParser());
-  // server.use(restifyValidator);
-
-  server.pre((req, res, next) => {
-    console.log(req.method, req.url);
-    next();
-  });
 
   server.get('/wines', APIwines.showAll);
   server.post('/wines', APIwines.insertOne);
